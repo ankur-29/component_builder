db68948bd2a65b72bdb24b7c94629266e603fc8c
ankur-29 <ankur.anand.me044@gmail.com>
2024-08-04 09:49:14 +0530

    created product and favourite page

---
+++ frontend/src/pages/Favourite.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+
+const Favourite = () => {
+  return (
+    <div>
+        <Navbar />
+        Favourites
+    </div>
+  )
+}
+
+export default Favourite
---
+++ frontend/src/pages/Project.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navbar from '../components/Navbar';
+
+const Project = () => {
+  return (
+    <div>
+        <Navbar />
+        Project
+    </div>
+  )
+}
+
+export default Project
